13e729283ccb8a4619ff20e6a495762426a480df
dbswjd2825 <157003346+dbswjd2825@users.noreply.github.com>
2024-03-18 14:33:16 +0900

    .

--- paulbassett/js/main.js
+++ paulbassett/js/main.js
@@ -47,18 +47,29 @@ $(document).ready(function(){
         slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
         spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
         breakpoints: {
-            768: {    /* 768px 이상일때 적용 */
+            500: {    /* 500px 이상일때 적용 */
                 slidesPerView: 2,
                 spaceBetween: 34,
             },
-            1024: {   /* 1024px 이상일때 적용 */
+            768: {    /* 768px 이상일때 적용 */
                 slidesPerView: 3,
                 spaceBetween: 34,
             },
-            1280: {    /* 1280px 이상일때 적용 */
+            1024: {   /* 1024px 이상일때 적용 */
                 slidesPerView: 4,
+                spaceBetween: 34,
+            },
+            1280: {    /* 1280px 이상일때 적용 */
+                slidesPerView: 5,
                 spaceBetween: 34,
             },
         },
     });
+
+    AOS.init({
+    offset: 300, // 해당 콘텐츠가 하단에서 몇 px 위로 올라와에 나타나는 효과가 나타날지 셋팅하는 값
+    duration: 500, // 애니메이션 효과가 작동되는 시간
+    easing: 'ease', // 가속도
+    });
+
 }) //$(document).ready
